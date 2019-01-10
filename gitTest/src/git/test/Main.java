4897b2334fea776a793efe266fc88175cdb8b3e4
user2 <user2@user2-PC>
2019-01-10 20:09:12 +0900

    이건충돌될꺼야!

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("추가한내용");
+		int age=19;
 		System.out.println("첫번쨰 수정내용");
 		System.out.println("이건 충돌나야돼!");
 
